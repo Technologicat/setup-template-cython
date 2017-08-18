deb6838f49732f0e5aaae0800ca2d29d76628a5e
Juha Jeronen <juha.jeronen@jyu.fi>
2017-08-18 15:24:33 +0300

    bump version

--- mylibrary/__init__.py
+++ mylibrary/__init__.py
@@ -5,7 +5,7 @@
 from __future__ import absolute_import
 
 # This is extracted automatically by the top-level setup.py.
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 # add any imports here, if you wish to bring things into the library's top-level namespace when the library is imported.
 
